750feed91ba61e4380255c8613f22ce21fbbc2db
santy0925 <santiagocardona0925@gmail.com>
2025-05-02 20:31:37 -0500

    rrrrrr

--- src/components/contenido.jsx
+++ src/components/contenido.jsx
@@ -1,15 +1,24 @@
 import {useContext  } from "react";
 import { ThemeContext } from "../ThemeContext";
 import { BotonCambioTema } from "./BotonCambioTema";
+import ListaTareas from "./ListaTareas";    
 
 export function Contenido() {
     const { tema } = useContext(ThemeContext);
+    const estilos = {
+        backgroundColor: tema === "claro" ? "#fff" : "#222",
+        color: tema === "claro" ? "#000" : "#fff",
+        height: "100vh",
+        padding: "2em",
+        transition: "all 0.3s",
+    };
 
     return (
         <div className={tema}>
-            <h1>Contenido</h1>
-            <p>Este es el contenido de la aplicación.</p>
-            <BotonCambioTema />
+            <h1>App con tema</h1>
+           <BotonCambioTema />
+           <h1>Lista de Tareas</h1>
+              <ListaTareas />
         </div>
     );
 }
